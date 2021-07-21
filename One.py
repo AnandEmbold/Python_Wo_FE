dd65deee0ccf6b589bd134b4ab8c57e25a32a6b9
AnandEmbold <80742162+AnandEmbold@users.noreply.github.com>
2021-07-21 14:16:57 +0530

    Add files via upload

---
+++ One.py
@@ -0,0 +1,7 @@
+class One:
+    @classmethod
+    def func1_one(cls):
+        ret: bool: True
+        
+    var1_one: str = 'test1'
+    
---
+++ Two.py
@@ -0,0 +1,5 @@
+def func2_two():
+    ret: bool: True
+    
+var2_two: str = 'test2'
+    
